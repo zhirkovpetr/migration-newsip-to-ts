592cbd405427a178ecdc5ebed564b9b00a4ab26b
Petr <zhirkovpetr95@gmail.com>
2023-11-16 17:42:29 +0300

    feat: add a slider for selecting news source

--- src/components/view/sources/sources.js
+++ src/components/view/sources/sources.js
@@ -1,21 +1,36 @@
-import './sources.css';
+import "./sources.css";
+import { ISource } from "../../../interface/IArticles";
 
 class Sources {
-    draw(data) {
+    draw(data: Array<ISource>) {
         const fragment = document.createDocumentFragment();
-        const sourceItemTemp = document.querySelector('#sourceItemTemp');
-
-        data.forEach((item) => {
+        const sourceItemTemp = document.querySelector("#sourceItemTemp");
+        data.forEach((item: ISource) => {
             const sourceClone = sourceItemTemp.content.cloneNode(true);
-
-            sourceClone.querySelector('.source__item-name').textContent = item.name;
-            sourceClone.querySelector('.source__item').setAttribute('data-source-id', item.id);
-
-            fragment.append(sourceClone);
+            if (sourceClone) {
+                sourceClone.querySelector(".source__item-name").textContent = item.name;
+                sourceClone.querySelector(".source__item").setAttribute("data-source-id", item.id);
+                fragment.append(sourceClone);
+            }
         });
-
-        document.querySelector('.sources').append(fragment);
+        document.querySelector(".sources").append(fragment);
     }
 }
+
+const s = document.querySelector(".sources");
+let o: number;
+o = 0;
+document.querySelector(".slider-next").addEventListener("click", function () {
+    o += 256;
+    s.style.left = -o + "px";
+});
+
+document.querySelector(".slider-prev").addEventListener("click", function () {
+    o -= 256;
+    if (o < 0) {
+        o = 0;
+    }
+    s.style.left = -o + "px";
+});
 
 export default Sources;
